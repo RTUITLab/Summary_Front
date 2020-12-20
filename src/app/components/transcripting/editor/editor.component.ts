5841bdaf6ca839b305cac9d0a241a513552b7bb3
IvLaptev <ivlaptev13@ya.ru>
2020-12-20 20:58:42 +0300

    Added printer

--- src/app/components/transcripting/editor/editor.component.ts
+++ src/app/components/transcripting/editor/editor.component.ts
@@ -16,6 +16,22 @@ export class EditorComponent implements OnInit {
       let btn = btns.item(i);
 
       btn.addEventListener('click', () => {
+        if (btn.dataset['command'] === 'print') {
+          var mywindow = window.open('', 'PRINT', 'height=400,width=600');
+
+          mywindow.document.write('<html><head><title>' + document.title  + '</title>');
+          mywindow.document.write('</head><body >');
+          mywindow.document.write('<h1>' + document.title  + '</h1>');
+          mywindow.document.write(document.getElementById('list').innerHTML);
+          mywindow.document.write('</body></html>');
+
+          mywindow.document.close(); // necessary for IE >= 10
+          mywindow.focus(); // necessary for IE >= 10*/
+
+          mywindow.print();
+          mywindow.close();
+          return;
+        }
         document.execCommand(btn.dataset['command'], false, null);
       })
     }
