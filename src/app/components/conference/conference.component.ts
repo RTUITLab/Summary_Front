08fd6af588b2a9b53658c2bd2471a1bdea079869
Nick <nikitamm59rus@gmail.com>
2021-02-21 13:15:41 +0300

    Add loading indicator

--- src/app/components/conference/conference.component.ts
+++ src/app/components/conference/conference.component.ts
@@ -11,6 +11,7 @@ import { MenuOptions, MenuOptionsService } from 'src/app/services/MenuOptions/me
 export class ConferenceComponent implements OnInit {
   state: ConferenceState;
   conferenceState = ConferenceState;
+  isLoading: boolean;
   constructor(private menuOptions: MenuOptionsService,
       private conferenceService: ConferenceService,
       private router: Router) { 
@@ -20,23 +21,32 @@ export class ConferenceComponent implements OnInit {
   ngOnInit(): void {
     this.menuOptions.setOption(MenuOptions.Conference);
     this.conferenceService.clearConference();
+    this.loading(false);
   }
 
   async create(conferenceName: string, organizatorName: string): Promise<void> {
+    this.loading(true);
     let conference = await this.conferenceService.createConference(conferenceName, organizatorName);
     if (conference === null) {
       alert("Ошибка во время создания конференции");
     }
-    this.router.navigate(["/room"])
+    this.loading(false);
+    this.router.navigate(["/room"]);;
   }
 
   async join(conferenceId: string, participantName: string): Promise<void> {
+    this.loading(true);
     let conference = await this.conferenceService.joinToConference(conferenceId, participantName);
     console.log(conference);
     if (conference === null) {
       alert("Ошибка во время подключения к конференции");
     }
+    this.loading(false);
     this.router.navigate(["/room"]);
+  }
+
+  loading(isLoading: boolean): void {
+    this.isLoading = isLoading;
   }
 }
 
